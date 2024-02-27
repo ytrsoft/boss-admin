d16be0b72b930582a363e0ab10786b3119913c56
ytrsoft <1654486219@qq.com>
2024-02-27 14:28:48 +0800

    init

--- src/_mock/account.js
+++
@@ -1,6 +0,0 @@
-// ----------------------------------------------------------------------
-
-export const account = {
-  displayName: 'Iceman',
-  photoURL: '/assets/images/avatars/avatar_25.jpg',
-};
